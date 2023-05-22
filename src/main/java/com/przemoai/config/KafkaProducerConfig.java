9cd11f71e8ddc1d7472354849b67a8e6c0f1887f
przemoai <again364@gmail.com>
2023-05-22 13:50:47 +0200

    Add KafkaTemplate

--- src/main/java/com/przemoai/config/KafkaProducerConfig.java
+++ src/main/java/com/przemoai/config/KafkaProducerConfig.java
@@ -3,8 +3,10 @@ package com.przemoai.config;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.core.DefaultKafkaProducerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.core.ProducerFactory;
 
 import java.util.HashMap;
@@ -25,8 +27,14 @@ class KafkaProducerConfig {
         return properties;
     }
 
+    @Bean
     ProducerFactory<String, String> producerFactory() {
         return new DefaultKafkaProducerFactory<>(producerConfig());
+    }
+
+    @Bean
+    KafkaTemplate<String, String> kafkaTemplate(ProducerFactory<String, String> producerFactory) {
+        return new KafkaTemplate<>(producerFactory);
     }
 
 }
